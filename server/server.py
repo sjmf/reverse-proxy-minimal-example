bf20f5480d37259c43bc41f815d4eac1c45a0992
Samantha Finnigan <samantha.finnigan@northumbria.ac.uk>
2021-02-22 11:47:45 +0000

    🐣 Further minify example

--- server/server.py
+++ server/server.py
@@ -2,26 +2,13 @@ from flask import Flask, url_for, Response, request
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-
 app.wsgi_app = ProxyFix(app.wsgi_app, x_host=1, x_port=1)
 
 
 @app.route("/")
 def index():
-    return """<pre>
-url_for("index", _external=True) = {}
-HTTP_X_FORWARDED_FOR = {}
-HTTP_X_FORWARDED_PROTO = {}
-HTTP_X_FORWARDED_HOST = {}
-HTTP_X_FORWARDED_PORT = {}
-HTTP_X_FORWARDED_PREFIX = {}
-        """.format(
-            url_for("index", _external=True),
-            request.headers.get("X_FORWARDED_FOR"),
-            request.headers.get("X_FORWARDED_PROTO"),
-            request.headers.get("X_FORWARDED_HOST"),
-            request.headers.get("X_FORWARDED_PORT"),
-            request.headers.get("X_FORWARDED_PREFIX"))
+    return "<pre>url_for('index', _external=True) = {}\n\n{}"\
+        .format(url_for('index', _external=True), request.headers)
 
 
 @app.route('/test', methods=['GET'])
