b7c8174c8397f90ad76b802540ded7c512c3e2a5
Samantha Finnigan <samantha.finnigan@northumbria.ac.uk>
2021-02-22 12:42:57 +0000

    🧸 Add back all ProxyFix params

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, Response, request
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app, x_host=1, x_port=1)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_port=1, x_proto=1, x_prefix=1)
 
 
 @app.route("/")
